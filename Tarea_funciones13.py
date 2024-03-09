dfeca1abf832eed51d4d341d94ae4bc3b247210e
https://github.com/DavidUEA/Tareagym.git <dh.changoa@uea.edu.ec>
2024-03-09 09:58:31 -0600

    Merge remote-tracking branch 'origin/main'

--- Tarea_funciones13.py
+++ Tarea_funciones13.py
@@ -1,37 +1,146 @@
 
-def temperatura_promedio(temperatura):
-    """
-    Esta función calcula la temperatura promedio de un conjunto de ciudades.
-    Args:
-        ciudades_temperaturas (dict): Un diccionario que contiene nombres de ciudades como claves
-                                      y listas de temperaturas como valores.
-    Returns:
-        dict: Un diccionario que contiene nombres de ciudades como claves
-              y temperaturas promedio como valores.
-    """
-    temperaturas_promedio = {}
+#Función  para calcular el promedio de temperaturas de una ciudad en un intervalo  de tiempo
 
-    for ciudad, temperaturas in temperatura.items():
-        promedio = sum(temperaturas) / len(temperaturas)
-        temperaturas_promedio[ciudad] = promedio
+def cal_temp(datos, ciudad, semana_inicio, semana_final):
+    num_items =0  # variable para almacenar el total de items Leidos
+    #sum_temp =0 # variatte que acumlarde la temperatura
+    for i in range(len(datos[ciudad])): #obtener numero de elementos en la ciudad
+        if i>=semana_inicio and i<=semana_final:
+            for j in range(len(datos[ciudad][i])):
+                sum_temp=sum_temp+datos[ciudad][i][j]["temp"]
+                num_items=num_items+1
 
-    return temperaturas_promedio
+                print(datos[ciudad][i][j])
+
+    promedio=sum_temp/num_items
+    return promedio
 
 
 # Creamos un diccionario de ciudades y temperaturas
-temperatura = {
-    "Nueva York": [22, 25, 26, 24, 23],
-    "Los Ángeles": [28, 30, 29, 31, 27],
-    "Chicago": [21, 20, 22, 18, 19],
-    "Miami": [32, 33, 34, 30, 32],
-    "Dallas": [26, 28, 27, 29, 25]}
+temperaturas = [
+    [   # Ciudad 1
+        [   # Semana 1
+            {"day": "lunes", "temp": 12},
+            {"day": "martes", "temp": 15},
+            {"day": "Miércoles", "temp": 15},
+            {"day": "Jueves", "temp": 19},
+            {"day": "Viernes", "temp": 20},
+            {"day": "Sábado", "temp": 16},
+            {"day": "Domingo", "temp": 17}
+        ],
+        [   # Semana 2
+            {"day": "Lunes", "temp": 14},
+            {"day": "Martes", "temp": 12},
+            {"day": "Miércoles", "temp": 16},
+            {"day": "Jueves", "temp": 15},
+            {"day": "Viernes", "temp": 12},
+            {"day": "Sábado", "temp": 14},
+            {"day": "Domingo", "temp": 13}
+        ],
+        [   # Semana 3
+            {"day": "Lunes", "temp": 13},
+            {"day": "Martes", "temp": 11},
+            {"day": "Miércoles", "temp": 12},
+            {"day": "Jueves", "temp": 14},
+            {"day": "Viernes", "temp": 17},
+            {"day": "Sábado", "temp": 20},
+            {"day": "Domingo", "temp": 19}
+        ],
+        [   # Semana 4
+            {"day": "Lunes", "temp": 16},
+            {"day": "Martes", "temp": 17},
+            {"day": "Miércoles", "temp": 14},
+            {"day": "Jueves", "temp": 14},
+            {"day": "Viernes", "temp": 12},
+            {"day": "Sábado", "temp": 13},
+            {"day": "Domingo", "temp": 15}
+        ]
+    ],
+    [   # Ciudad 2
+        [   # Semana 1
+            {"day": "Lunes", "temp": 17},
+            {"day": "Martes", "temp": 15},
+            {"day": "Miércoles", "temp": 20},
+            {"day": "Jueves", "temp": 21},
+            {"day": "Viernes", "temp": 20},
+            {"day": "Sábado", "temp": 15},
+            {"day": "Domingo", "temp": 17}
+        ],
+        [   # Semana 2
+            {"day": "Lunes", "temp": 13},
+            {"day": "Martes", "temp": 16},
+            {"day": "Miércoles", "temp": 17},
+            {"day": "Jueves", "temp": 17},
+            {"day": "Viernes", "temp": 15},
+            {"day": "Sábado", "temp": 12},
+            {"day": "Domingo", "temp": 18}
+        ],
+        [   # Semana 3
+            {"day": "Lunes", "temp": 16},
+            {"day": "Martes", "temp": 15},
+            {"day": "Miércoles", "temp": 18},
+            {"day": "Jueves", "temp": 20},
+            {"day": "Viernes", "temp": 17},
+            {"day": "Sábado", "temp": 18},
+            {"day": "Domingo", "temp": 15}
+        ],
+        [   # Semana 4
+            {"day": "Lunes", "temp": 14},
+            {"day": "Martes", "temp": 17},
+            {"day": "Miércoles", "temp": 19},
+            {"day": "Jueves", "temp": 14},
+            {"day": "Viernes", "temp": 13},
+            {"day": "Sábado", "temp": 15},
+            {"day": "Domingo", "temp": 18}
+        ]
+    ],
+    [   # Ciudad 3
+        [   # Semana 1
+            {"day": "Lunes", "temp": 25},
+            {"day": "Martes", "temp": 28},
+            {"day": "Miércoles", "temp": 24},
+            {"day": "Jueves", "temp": 25},
+            {"day": "Viernes", "temp": 26},
+            {"day": "Sábado", "temp": 28},
+            {"day": "Domingo", "temp": 30}
+        ],
+        [   # Semana 2
+            {"day": "Lunes", "temp": 29},
+            {"day": "Martes", "temp": 31},
+            {"day": "Miércoles", "temp": 27},
+            {"day": "Jueves", "temp": 28},
+            {"day": "Viernes", "temp": 27},
+            {"day": "Sábado", "temp": 29},
+            {"day": "Domingo", "temp": 24}
+        ],
+        [   # Semana 3
+            {"day": "Lunes", "temp": 25},
+            {"day": "Martes", "temp": 29},
+            {"day": "Miércoles", "temp": 27},
+            {"day": "Jueves", "temp": 28},
+            {"day": "Viernes", "temp": 29},
+            {"day": "Sábado", "temp": 30},
+            {"day": "Domingo", "temp": 33}
+        ],
+        [   # Semana 4
+            {"day": "Lunes", "temp": 24},
+            {"day": "Martes", "temp": 26},
+            {"day": "Miércoles", "temp": 25},
+            {"day": "Jueves", "temp": 28},
+            {"day": "Viernes", "temp": 29},
+            {"day": "Sábado", "temp": 30},
+            {"day": "Domingo", "temp": 31}
+        ]
+    ]
+]
 
-
+ciudad= input("ingrese su ciudad:")
+semana_inicio = input("ingrese semana inicio:")
+semana_final = input("ingrese semana final:")
 
 # Llamamos a la función para calcular las temperaturas promedio
-temperaturas_promedio = temperatura_promedio(temperatura)
+promediof= cal_temp=(temperaturas, ciudad, semana_inicio, semana_final)
+print(promediof)
 
-# Mostramos los resultados
-print("Temperaturas Promedio por Ciudad:")
-for ciudad, promedio in temperaturas_promedio.items():
-    print(f"{ciudad}: {promedio:.2f}°C")
+
+
