63945f00daeb6f99f9d15bff3b90a12d2b06998f
https://github.com/DavidUEA/Tareagym.git <dh.changoa@uea.edu.ec>
2024-03-14 17:28:59 -0600

    Tarea 14

--- Calcular_descuento.py
+++ Calcular_descuento.py
@@ -1,20 +1,19 @@
 
 
 
-def calcular_descuento(descuento,compra1):
+def calcular_descuento(compra1, descuento=10):
     suma_com= descuento * compra1/100
-
     return  suma_com
 
 
 
 compra1= float(input("ingrese su valor total de la compra:"))
-descuento= float(input("ingrese su valor del descuento:"))
 
-valor= calcular_descuento(compra1,descuento)
 
+valor= calcular_descuento(compra1,20)
+descuento= 20
 valor_final = compra1 - valor
 
 
 print("El valor de su descuento es:", valor,)
-print("Su compra de:", compra1, "con descuento del", descuento,"%" " tiene el descuento de:", valor_final)
+print("Su compra de:", compra1, "con descuento del", descuento,"% , "" Su pago total es de:", valor_final)
