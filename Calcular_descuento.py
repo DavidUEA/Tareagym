cc0d626553df5f0383297ffff1ca5dadbfd93259
https://github.com/DavidUEA/Tareagym.git <dh.changoa@uea.edu.ec>
2024-03-16 10:20:46 -0600

    Merge remote-tracking branch 'origin/main'

--- Calcular_descuento.py
+++ Calcular_descuento.py
@@ -12,9 +12,11 @@ compra1= float(input("ingrese su valor total de la compra:"))
 
 valor= calcular_descuento(compra1,20)
 descuento= 20
+
 valor_final = compra1 - valor
 
-#descuent1= calcular_descuento(valor_final)
+
+
 
 print("El valor de su descuento es:", valor,)
 print("Su compra de:", compra1, "con descuento del", descuento,"% , "" Su pago total es de:", valor_final)
