5e47431a330f23fbd94cc0fa69b88a5d2c4e5bba
https://github.com/DavidUEA/Tareagym.git <dh.changoa@uea.edu.ec>
2024-03-31 12:43:12 -0600

    tarea semana 16

--- TAREA16/lectura_escrituras.py
+++ TAREA16/lectura_escrituras.py
@@ -1,18 +1,27 @@
+#abrir el archivo modo lectura
 archivo = open("my_notes.txt", "r")
+#lectura del archivo por lineas
 notes = archivo.readline()
 notes1 = archivo.readline()
 notes2 = archivo.readline()
-
+#imprimir el archivo por lineas
 print(notes)
 print(notes1)
 print(notes2)
+#cerramos el archivo
 archivo.close()
-
+#cerramos el archivo modo escritura para
+#ingresar un dato
 archivo = open("my_notes.txt", "a")
+#dato a ingresar
 linea = ["EMAIL: DCHANGO90.DC"]
 archivo.writelines(linea)
+#imprimimos el archivo con el dato ingresado
+#sin borrar los archivos que se encontraban
+#en el texto
 print(archivo)
 archivo.close()
+#lectura del archivo con el dato agregado
 archivo = open("my_notes.txt", "r")
 nota4= archivo.read()
 print(nota4)
